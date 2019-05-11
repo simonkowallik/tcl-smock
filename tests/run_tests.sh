5ba569bba514308613a949fe0e8332e334b0893a
Simon Kowallik <github@simonkowallik.com>
2019-05-11 14:49:19 +0200

    test tcl 8.4, 8.5 and 8.6

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-_tclsh=$(command -v tclsh8.4)
-_tclsh=${_tclsh:-tclsh}
+_tclsh84=$(command -v tclsh8.4)
+_tclsh85=$(command -v tclsh8.5)
+_tclsh86=$(command -v tclsh8.6)
 
 set -ev
 
-$_tclsh tests/test.tcl
+$_tclsh84 tests/test.tcl
+
+$_tclsh85 tests/test.tcl
+
+$_tclsh86 tests/test.tcl
